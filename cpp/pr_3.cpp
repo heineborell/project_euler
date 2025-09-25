773c63f72190191880cead6f72a7b7763587f3d1
Deniz Deveci <dodeve@gmail.com>
2025-09-25 12:38:03 +0900

    cpp 4

--- cpp/pr_3.cpp
+++ cpp/pr_3.cpp
@@ -1,6 +1,7 @@
 #include <chrono> // for std::chrono functions
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 class Timer {
 private:
@@ -18,11 +19,58 @@ public:
   }
 };
 
+struct Pair {
+  long first{};
+  long second{};
+};
+
+bool checkPrime(long n) {
+  for (long m{2}; static_cast<long double>(m) < (std::sqrt(n) + 1); ++m) {
+    if (n % m == 0)
+      return false;
+  }
+  return true;
+}
+
+Pair divide(long n, long m) {
+  if (n % m == 0 && checkPrime(m))
+    return {n / m, m};
+  else
+    return {};
+}
+
+std::vector<Pair> largestPrime(long n) {
+  std::vector<Pair> divisorlist{};
+  // Handle the multiplies of 2
+  while (n % 2 == 0) {
+    divisorlist.push_back(divide(n, 2));
+    n = n / 2;
+  }
+
+  for (int m{3}; static_cast<long double>(m) < (std::sqrt(n) + 1); m += 2) {
+    while (n % m == 0) {
+      divisorlist.push_back(divide(n, m));
+      n = n / m;
+    }
+  }
+
+  if (n > 2)
+    divisorlist.push_back({1, n});
+  return divisorlist;
+}
+
+template <typename T> void printArray(const std::vector<T> &array) {
+  for (T element : array) {
+    if (element.first != 0)
+      std::cout << '{' << element.first << ',' << element.second << '}' << '\n';
+  }
+}
+
 int main() {
   Timer t;
-
-  std::cout << typeid(std::sqrt(4)).name();
-
+  std::cout << std::boolalpha;
+  printArray(largestPrime(600851475143));
+  // std::cout << largestPrime(600851475143).back().second << '\n';
   std::cout << "Time elapsed: " << t.elapsed() << " seconds\n";
 
   return 0;
